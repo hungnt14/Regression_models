6c58383653ec5482aa261cde2b001cbfc281353f
Cao Văn Hùng <79310692+hungcao0402@users.noreply.github.com>
2023-01-05 12:51:20 +0700

    Update getter.py

--- utils/getter.py
+++ utils/getter.py
@@ -3,7 +3,7 @@ import sys
 sys.path.append(".")
 
 from models.LinearSGD import *
-# from models.DNN import *
+from models.DNN import *
 from models.DecisionTree import *
 from models.RidgeRegression import *
 from models.LassoRegression import *
