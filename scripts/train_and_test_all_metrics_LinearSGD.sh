c6c082dcd9796a71a908755c647c303df3f40bfa
Nguyen Tien Hung <nguyentienhung.quynhon@gmail.com>
2023-01-07 19:31:56 +0700

    update config for LinearSGD and add lightgbm to requirements.txt

--- scripts/train_and_test_all_metrics_LinearSGD.sh
+++ scripts/train_and_test_all_metrics_LinearSGD.sh
@@ -1,6 +1,6 @@
-python3 -W ignore tools/train.py --val_metric MAPE --weight_path weights/LinearSGD_MAPE.pkl
-python3 -W ignore tools/test.py --test_metric MAPE --weight_path weights/LinearSGD_MAPE.pkl
-python3 -W ignore tools/train.py --val_metric RMSE --weight_path weights/LinearSGD_RMSE.pkl
-python3 -W ignore tools/test.py --test_metric RMSE --weight_path weights/LinearSGD_RMSE.pkl
-python3 -W ignore tools/train.py --val_metric MAE --weight_path weights/LinearSGD_MAE.pkl
-python3 -W ignore tools/test.py --test_metric MAE --weight_path weights/LinearSGD_MAE.pkl
+python3 -W ignore tools/train.py --val_metric MAPE --weight_path weights/LinearSGD_MAPE.pkl --preprocess_data 0
+python3 -W ignore tools/test.py --test_metric MAPE --weight_path weights/LinearSGD_MAPE.pkl --preprocess_data 0
+python3 -W ignore tools/train.py --val_metric RMSE --weight_path weights/LinearSGD_RMSE.pkl --preprocess_data 0
+python3 -W ignore tools/test.py --test_metric RMSE --weight_path weights/LinearSGD_RMSE.pkl --preprocess_data 0
+python3 -W ignore tools/train.py --val_metric MAE --weight_path weights/LinearSGD_MAE.pkl --preprocess_data 0
+python3 -W ignore tools/test.py --test_metric MAE --weight_path weights/LinearSGD_MAE.pkl --preprocess_data 0
