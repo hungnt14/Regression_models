15dd48d3e2089c8249f58852012ab05577f73a9b
vinhhung053 <vinhhung053@gmail.com>
2023-01-08 23:12:20 +0700

    update ElasticNet

--- scripts/train_and_test_all_metrics_ElasticNet.sh
+++ scripts/train_and_test_all_metrics_ElasticNet.sh
@@ -1,6 +1,6 @@
-python3 -W ignore tools/train.py --val_metric MAPE --weight_path weights/ElasticNet._MAPE.pkl
-python3 -W ignore tools/test.py --test_metric MAPE --weight_path weights/ElasticNet._MAPE.pkl
-python3 -W ignore tools/train.py --val_metric RMSE --weight_path weights/ElasticNet._RMSE.pkl
-python3 -W ignore tools/test.py --test_metric RMSE --weight_path weights/ElasticNet._RMSE.pkl
-python3 -W ignore tools/train.py --val_metric MAE --weight_path weights/ElasticNet._MAE.pkl
-python3 -W ignore tools/test.py --test_metric MAE --weight_path weights/ElasticNet._MAE.pkl
+python3 -W ignore tools/train.py --config_path configs/ElasticNet.yml --val_metric MAPE --weight_path weights/ElasticNet_MAPE
+python3 -W ignore tools/test.py --config_path configs/ElasticNet.yml --test_metric MAPE --weight_path weights/ElasticNet_MAPE
+python3 -W ignore tools/train.py --config_path configs/ElasticNet.yml --val_metric RMSE --weight_path weights/ElasticNet_RMSE
+python3 -W ignore tools/test.py --config_path configs/ElasticNet.yml --test_metric RMSE --weight_path weights/ElasticNet_RMSE
+python3 -W ignore tools/train.py --config_path configs/ElasticNet.yml --val_metric MAE --weight_path weights/ElasticNet_MAE
+python3 -W ignore tools/test.py --config_path configs/ElasticNet.yml --test_metric MAE --weight_path weights/ElasticNet_MAE
